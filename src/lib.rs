c0d59d30bdefdd6216c72b212520131948262f92
Matyas Susits <susits.matyas@gmail.com>
2022-12-12 09:32:11 +0100

    add order_multiple_with_dir

--- src/lib.rs
+++ src/lib.rs
@@ -73,6 +73,25 @@ impl QueryOrderDirection {
         }
     }
 
+    pub fn order_multiple<Expr, Q>(
+        &self,
+        query: Q,
+        expr: Expr,
+    ) -> OrderMultipleWithDirection<Q, Expr>
+    where
+        Expr: OrderableTuple,
+        Q: OrderDsl<<Expr as OrderableTuple>::AscOutput>
+            + OrderDsl<
+                <Expr as OrderableTuple>::DescOutput,
+                Output = <Q as OrderDsl<<Expr as OrderableTuple>::AscOutput>>::Output,
+            >,
+    {
+        match self {
+            Self::Ascending => query.order(OrderableTuple::asc(expr)),
+            Self::Descending => query.order(OrderableTuple::desc(expr)),
+        }
+    }
+
     /// Append a [`QueryDsl::then_order_by`](diesel::QueryDsl::then_order_by) to the query. Same as
     /// [`query.then_order_by_with_dir(self, expr)`](OrderWithDirectionDsl::then_order_by_with_dir)
     pub fn then_order_by_query<Expr, Q>(
@@ -94,6 +113,9 @@ impl QueryOrderDirection {
 
 /// Represent the return type of [`.order_with_dir`](`OrderWithDirectionDsl::order_with_dir`)
 pub type OrderWithDirection<Q, Expr> = <Q as OrderDsl<Asc<Expr>>>::Output;
+/// Represent the return type of [`.order_multiple_with_dir`](`OrderWithDirectionDsl::order_with_dir`)
+pub type OrderMultipleWithDirection<Q, Expr> =
+    <Q as OrderDsl<<Expr as OrderableTuple>::AscOutput>>::Output;
 /// Represent the return type of [`.then_order_by_with_dir`](`OrderWithDirectionDsl::then_order_by_with_dir`)
 pub type ThenOrderByWithDirection<Q, Expr> = <Q as ThenOrderDsl<Asc<Expr>>>::Output;
 
@@ -127,6 +149,47 @@ pub trait OrderWithDirectionDsl: Sized {
         self.order_with_dir(order, expr)
     }
 
+    /// Wrapper around [`QueryDsl::order`](diesel::QueryDsl::order) when its `expr` is a tuple. The `direction` argument determines the ordering direction of all fields in the tuple
+    /// ```
+    /// # #[macro_use] extern crate diesel;
+    /// # use diesel::mysql::Mysql;
+    /// # use diesel::{QueryDsl, ExpressionMethods};
+    /// # use diesel_order_with_direction::{OrderWithDirectionDsl, QueryOrderDirection};
+    /// # table! {
+    /// #     people (id) {
+    /// #         id -> Integer,
+    /// #         age -> Integer,
+    /// #         height -> Integer,
+    /// #     }
+    /// # }
+    /// // These two do the same
+    /// people::table
+    ///     .into_boxed()
+    ///     .order((people::height.asc(), people::age.asc()));
+    /// people::table
+    ///     .into_boxed()
+    ///     .order_multiple_with_dir(
+    ///         QueryOrderDirection::Ascending,
+    ///         (people::height, people::age),
+    ///      );
+    /// ```
+    /// dynamically determine the ordering direction. Only works with boxed queries.
+    fn order_multiple_with_dir<Expr>(
+        self,
+        direction: QueryOrderDirection,
+        expr: Expr,
+    ) -> OrderMultipleWithDirection<Self, Expr>
+    where
+        Expr: OrderableTuple,
+        Self: OrderDsl<<Expr as OrderableTuple>::AscOutput>
+            + OrderDsl<
+                <Expr as OrderableTuple>::DescOutput,
+                Output = <Self as OrderDsl<<Expr as OrderableTuple>::AscOutput>>::Output,
+            >,
+    {
+        direction.order_multiple(self, expr)
+    }
+
     /// Wrapper around [`QueryDsl::then_order_by`](diesel::QueryDsl::then_order_by) with an extra [`QueryOrderDirection`] argument to
     /// dynamically determine the ordering direction. Only works with boxed queries.
     fn then_order_by_with_dir<Expr>(
@@ -144,3 +207,45 @@ pub trait OrderWithDirectionDsl: Sized {
 }
 
 impl<Q> OrderWithDirectionDsl for Q {}
+
+pub trait OrderableTuple {
+    type AscOutput: diesel::Expression;
+    type DescOutput: diesel::Expression;
+    fn asc(self) -> Self::AscOutput;
+    fn desc(self) -> Self::DescOutput;
+}
+
+macro_rules! impl_orderabletuple {
+    ($($G:ident),+) => {
+        impl<$($G: ExpressionMethods,)*> OrderableTuple for ($($G,)*) {
+            type AscOutput = ($(Asc<$G>,)*);
+            type DescOutput = ($(Desc<$G>,)*);
+            fn asc(self) -> Self::AscOutput {
+                #[allow(non_snake_case)]
+                let ($($G,)*) = self;
+                ($($G.asc(),)*)
+            }
+            fn desc(self) -> Self::DescOutput {
+                #[allow(non_snake_case)]
+                let ($($G,)*) = self;
+                ($($G.desc(),)*)
+            }
+        }
+    }
+}
+
+impl_orderabletuple! {A1}
+impl_orderabletuple! {A1,A2}
+impl_orderabletuple! {A1,A2,A3}
+impl_orderabletuple! {A1,A2,A3,A4}
+impl_orderabletuple! {A1,A2,A3,A4,A5}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14}
+impl_orderabletuple! {A1,A2,A3,A4,A5,A6,A7,A8,A9,A10,A11,A12,A13,A14,A15}
